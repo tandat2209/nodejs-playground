dbeef0f5bdeac4f0c5cae79d47769bda5aae4ab3
Tan Dat <tandat2209@gmail.com>
2019-07-31 10:08:16 +0700

    metrics & dimensions

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,12 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
 const gaServices = require('../services/ga');
 
-exports.getAnalytics = (req, res) => {
+router.get('/', (req, res) => {
   const basicReport = {
     reportRequests: [
       {
         viewId: '194627569',
         dateRanges: [{ startDate: '30daysAgo', endDate: 'today' }],
-        metrics: [{ expression: 'ga:users' }]
+        metrics: [{ expression: 'ga:sessions' }],
+        dimensions: [{ name: 'ga:date' }]
       }
     ]
   };
@@ -14,4 +18,41 @@ exports.getAnalytics = (req, res) => {
   gaServices.getReports(basicReport)
     .then(data => res.json(data))
     .catch(e => res.status(404, e));
-};
+});
+
+router.get('/visits', (req, res) => {
+  const visitReport = {
+    reportRequests: [
+      {
+        viewId: '194627569',
+        dateRanges: [{ startDate: '30daysAgo', endDate: 'today' }],
+        metrics: [{ expression: 'ga:users' }, { expression: 'ga:newUsers' }],
+      }
+    ]
+  };
+
+  gaServices.getReports(visitReport)
+    .then(data => res.json(data))
+    .catch(e => res.status(404, e));
+});
+
+
+router.get('/devices', (req, res) => {
+  const deviceReport = {
+    reportRequests: [
+      {
+        viewId: '194627569',
+        dateRanges: [{ startDate: '30daysAgo', endDate: 'today' }],
+        metrics: [{ expression: 'ga:users' }],
+        dimensions: [{ name: 'ga:deviceCategory' }]
+      }
+    ]
+  };
+
+  gaServices.getReports(deviceReport)
+    .then(data => res.json(data))
+    .catch(e => res.status(404, e));
+});
+
+
+module.exports = router;
